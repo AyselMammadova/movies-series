613299b43d1e65ad4ee9e1ec3f445dc9f958d0fe
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2025-10-21 19:20:08 +0400

    test

--- components/pages/Home/GoldenGlobe.tsx
+++ components/pages/Home/GoldenGlobe.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const GoldenGlobe = () => {
     return (
-        <section className='max-md:my-7 md:my-30 max-md:py-7 max-md:pl-7 md:py-15 md:pl-10 bg-goldenrod relative before:absolute before:top-0 before:left-0 before:w-full before:h-19 before:bg-[linear-gradient(to_bottom,_goldenrod_0%,_goldenrod_80%,_rgba(0,0,0,0.1)_100%)]'>
+        <section className='max-md:my-7 md:my-30 max-md:py-7 max-md:pl-7 md:py-15 md:pl-10 bg-goldenrod relative before:absolute before:-top-7 before:left-0 before:w-full before:h-7 before:bg-[linear-gradient(to_bottom,_rgba(0,0,0,0.2),_rgba(0,0,0,0.1)_20%,_goldenrod_100%)]
+        after:absolute after:-bottom-7 after:left-0 after:w-full after:h-7 after:bg-[linear-gradient(to_top,_rgba(0,0,0,0.2),_rgba(0,0,0,0.1)_20%,_goldenrod_100%)]'>
             <div className="flex items-center">
                 <Image src={'/images/icons/golden-globe.svg'} alt='golden globe' width={639} height={402} className='w-2/5' />
                 <figure className='w-3/5 relative before:absolute before:left-0 before:top-0 before:w-20 before:h-full before:bg-[linear-gradient(to_right,_goldenrod_0%,_goldenrod_80%,_rgba(0,0,0,0.1)_100%)]'>
