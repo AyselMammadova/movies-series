6d8d4a47bd3282ab01469262c376a8a7e7cf2480
Aysel Mammadova <ayselmemmedova9317@gmail.com>
2025-10-24 11:01:07 +0400

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import GoldenGlobe from "@/components/pages/Home/GoldenGlobe";
 import Hero from "@/components/pages/Home/Hero";
 import List from "@/components/pages/Home/List";
-import Subscribe from "@/components/pages/Home/Collection";
+// import Subscribe from "@/components/pages/Home/Collection";
 
 export default function HomePage() {
   return(
@@ -11,7 +11,7 @@ export default function HomePage() {
         <List type="Movies" />
         <GoldenGlobe />
         <List type="Series" />
-        <Subscribe />
+        {/* <Subscribe /> */}
       </>
   )
 }
